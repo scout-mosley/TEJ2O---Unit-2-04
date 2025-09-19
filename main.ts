32b1cf291a4bc9ffa03e03e3167ef8e0a8f2cca4
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-09-19 13:20:54 -0400

    when button A pressed shows temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: scout mosley
+ * Created on: Sep 2025
+ * This program shows temapture when A button pressed
 */
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
 
-basic.showString('Hello, World!')
+
+// when A button pressed tempature is shown
+let Tempature: number
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString('The Temperature is:' + input.temperature() )
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+
+})
+
